7d1c3cc5b5d3f7c8e74ba398a5c81c74eee992fa
zolov <i.zolov@yandex.ru>
2024-08-18 16:52:29 +0300

    # Arrays - Contains duplicate - Group anagram - Two sum - Valid anagram

---
+++ src/main/java/ru/zolov/arrays/TwoSumSolution.java
@@ -0,0 +1,46 @@
+package ru.zolov.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+ * You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+ * Return the answer with the smaller index first.
+ *
+ * @author skaiur
+ */
+public class TwoSumSolution {
+
+    static int[] twoSum(int[] nums, int target) {
+        // создаем словарь для хранения
+        // key: число
+        // value: индекс числа
+        var prevMap = new HashMap<Integer, Integer>();
+
+        // для всех значений в массиве
+        for (var i = 0; i < nums.length; i++) {
+            var num = nums[i];
+            // вычитаем текущее число из искомого числа для получения разности
+            var diff = target - num;
+
+            // проверяем что такое число (разность) представлена в словаре
+            if (prevMap.containsKey(diff)) {
+                // если это так, то возвращаем массив с индексом разности и индексом текущего числа
+                // чисел которые вместе дают искомую сумму
+                return new int[]{prevMap.get(diff), i};
+            }
+            // добавляем в словарь текущее число (вычитаемое) как ключ и его индекс как значение
+            prevMap.put(num, i);
+        }
+
+        // если не нашли числа которые вместе будут равны искомому числу возвращаем пустой массив
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        var target = 9;
+        var numbers = new int[]{2, 7, 5, 8, 11};
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+}
---
+++ src/main/java/ru/zolov/arrays/ValidAnagramSolution.java
@@ -0,0 +1,62 @@
+package ru.zolov.arrays;
+
+import java.util.HashMap;
+
+/**
+ * Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+ * An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+ *
+ * @author skaiur
+ */
+public class ValidAnagramSolution {
+    static boolean isAnagram(String s, String t) {
+        // проверяем что длинна строк совпадает, иначе это не анаграмма
+        if (s.length() != t.length()) return false;
+
+        // создаем два словаря в которых будем хранить счетчики частотности букв в строках
+        var countS = new HashMap<Character, Integer>();
+        var countT = new HashMap<Character, Integer>();
+
+        for (var i = 0; i < s.length(); i++) {
+            // увеличиваем счетчик каждый раз когда встречается буква
+            countS.merge(s.charAt(i), 1, Integer::sum);
+            countT.merge(t.charAt(i), 1, Integer::sum);
+            // так же можно уменьшать счетчик в единственном словаре и потом проверять что все значения равны 0
+        }
+
+        // сравниваем словари с буквами
+        return countS.equals(countT);
+    }
+
+    static boolean isAnagram2(String s, String t) {
+        // проверяем что длинна строк совпадает, иначе это не анаграмма
+        if (s.length() != t.length()) return false;
+
+        // инициализируем массив для алфавита
+        var store = new int[26];
+        for (var i = 0; i < s.length(); i++) {
+            // у буквы первой буквы алфавита a 97й номер в ASKII таблице
+            // b = 98 и так далее
+            // вычитая из номера текущего символа 97 мы получаем его номер в алфавите
+            // увеличиваем счетчик частотности символа в массиве с алфавитом
+            store[s.charAt(i) - 'a']++;
+            // уменьшаем счетчик
+            store[t.charAt(i) - 'a']--;
+        }
+
+        // проверяем что все счетчики в массиве с алфавитов равны 0 иначе две сравниваемые строки не являются анаграммами
+        for (var n : store) {
+            if (n != 0) return false;
+        }
+
+        // возвращаем true
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("abc", "cba"));
+        System.out.println(isAnagram("xx", "x"));
+        System.out.println(isAnagram2("abc", "cba"));
+        System.out.println(isAnagram2("xx", "x"));
+    }
+}
